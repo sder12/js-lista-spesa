29c2d9adc142cb3fa85f473be96398c077a80c6b
sder12 <sara.cetto@gmail.com>
2022-10-13 13:49:44 +0200

    Empty layout

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Lista della spesa con ciclo while
+//Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo while.
+//- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+//- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
