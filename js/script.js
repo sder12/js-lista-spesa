478e2a8e087e7d9bd6999a7d01fe65a2c3b3e075
sder12 <sara.cetto@gmail.com>
2022-10-13 13:57:38 +0200

    Exercise complete

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 //Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo while.
 //- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 //- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+const spesa = ["uova", "latte", "miele", "mela","insalata","pasta","riso","sale"];
+console.log(spesa)
+
+let i = 0
+while (i < spesa.length){
+    console.log(spesa[i])
+    document.writeln(spesa[i]);
+    i++;
+}
